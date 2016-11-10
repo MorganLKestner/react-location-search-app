d5d7aedb23e61a9ca41ba96f2ae884d27b7e8d95
Morgan Kestner <morganleekestner@gmail.com>
2016-11-09 23:53:31 -0500

    added main.js and packages

---
+++ components/Map.js
@@ -0,0 +1,42 @@
+var React = require('react');
+
+var Map = React.createClass({
+
+    componentDidMount() {
+
+        this.componentDidUpdate();
+    },
+
+    componentDidUpdate() {
+
+        if (this.lastLat == this.props.lat && this.lastLng == this.props.lng) {
+
+            return;
+        }
+
+        this.lastLat = this.props.lat;
+        this.lastLng = this.props.lng
+
+        var map = new GMaps({
+            el: '#map',
+            lat: this.props.lat,
+            lng: this.props.lng
+        });
+
+        map.addMarker({
+            lat: this.props.lat,
+            lng: this.props.lng
+        });
+    },
+
+    render() {
+        return (
+            <div className="map-holder">
+                <p>Loading...</p>
+                <div id="map"></div>
+            </div>
+        );
+    }
+});
+
+module.exports = Map;
